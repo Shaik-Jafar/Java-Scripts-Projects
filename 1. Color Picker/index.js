9f18f67561c86bde9b0ba6789527bec9c9345502
Shaik-Jafar <79200391+Shaik-Jafar@users.noreply.github.com>
2024-10-25 19:28:41 +0530

    Create index.js

---
+++ 1. Color Picker/index.js
@@ -0,0 +1,22 @@
+let backgd = document.getElementById("colorPickerContainer");
+let textC = document.getElementById("selectedColorHexCode");
+
+function bt1() {
+    backgd.style.backgroundColor = "#e0e0e0";
+    textC.textContent = "#e0e0e0";
+}
+
+function bt2() {
+    backgd.style.backgroundColor = "#6fcf97";
+    textC.textContent = "#6fcf97";
+}
+
+function bt3() {
+    backgd.style.backgroundColor = "#56ccf2";
+    textC.textContent = "#56ccf2";
+}
+
+function bt4() {
+    backgd.style.backgroundColor = "#bb6bd9";
+    textC.textContent = "#bb6db9";
+}
